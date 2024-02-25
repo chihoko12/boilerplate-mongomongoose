598db5072afd5dd2fff7cf6e268a8d7fd818f1b1
Chihoko Neu <chihoko.ueno@gmail.com>
2024-02-25 15:41:24 +0000

    use model.findOne() to return a single matching document from a database

--- myApp.js
+++ myApp.js
@@ -66,7 +66,13 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+
+  Person.findOne( { favoriteFoods:[food] }, (err, data) => {
+    if(err) {
+      return done(err);
+    }
+    done(null,data);
+  })
 };
 
 const findPersonById = (personId, done) => {
